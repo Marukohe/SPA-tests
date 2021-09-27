cb0a1f8e6a3f1281bb365a71e39c69456a0bdfbf
Wei He <1502449818@qq.com>
2021-09-27 21:01:55 +0800

    add readme

---
+++ JunitTest/LiveVarSPATests.java
@@ -0,0 +1,22 @@
+package pascal.taie.analysis.dataflow.analysis;
+
+import org.junit.Test;
+import pascal.taie.analysis.Tests;
+
+public class LiveVarSPATests {
+
+    static void testLV(String inputClass) {
+        Tests.testDFA(inputClass, "src/test/resources/SPA-tests",
+                LiveVariableAnalysis.ID);
+    }
+
+    @Test
+    public void testA() {
+        testLV("TestA");
+    }
+
+    @Test
+    public void testB() {
+        testLV("TestB");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# 软件分析实验测试
+
+## 使用方法
+
+```
+cd <path>/<to>/<tai-e>
+git submodule add https://github.com/Marukohe/SPA-tests.git src/test/resources/SPA-tests
+cp src/test/resources/SPA-tests/JunitTest/LiveVarSPATests.java src/test/java/pascal/tai/analysis/dataflow/analysis
+```
