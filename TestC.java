04a90c5e95e0b9f006c65d36684595e81e8ff32f
Lingyu Zhang <dz21330043@smail.nju.edu.cn>
2021-10-21 12:33:13 +0800

    add TestC and TestD

---
+++ TestC.java
@@ -0,0 +1,12 @@
+public class TestC {
+    void assignmentTests(){
+        int a = 1;
+        int b = use(a);
+        int c = a + 1;
+        c = 0;
+    }
+
+    void use(int x){
+
+    }
+}
---
+++ TestD.java
@@ -0,0 +1,12 @@
+public class TestD {
+    void Loops(){
+        for(int i = 0; i < 10; ++i){
+            --i;
+        }
+        useless();
+    }
+
+    void useless(){
+
+    }
+}
